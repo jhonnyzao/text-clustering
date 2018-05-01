8cade68a7b94f29814481af303373d879aea5544
Joao Armando <joaoarmando94@gmail.com>
2018-05-01 03:28:33 -0300

    Tem um bug que ta mandando um NaN pro calculo de distancia

--- teste.py
+++ teste.py
@@ -1,6 +1,7 @@
 import csv
 from collections import defaultdict
 import math
+import numpy
 
 iteracoes_maximas = 1000
 convergencia = 0.01
@@ -22,9 +23,11 @@ def distancia_euclidiana(centroide, dado):
 	valores_dado = dado
 
 	for indice, c in valores_centroides.items():
+		print(c, valores_dado[indice])
 		total += (int(c) - int(valores_dado[indice]))**2
 
 	total = total**0.5
+	total = round(total, 2)
 	return total
 
 
@@ -39,16 +42,33 @@ with open('textos.csv') as arquivo:
 
 centroides = inicializa_centroides(dados, total_k)
 
-matriz_distancias = defaultdict(dict)
-for i, centroide in centroides.items():
-	for j, dado in dados.items():
-		matriz_distancias[i][j] = distancia_euclidiana(centroide, dado)
+for m in range(0, 10):
+	matriz_distancias = defaultdict(dict)
+	for i, centroide in centroides.items():
+		for j, dado in dados.items():
+			matriz_distancias[i][j] = distancia_euclidiana(centroide, dado)
 
-grupos = defaultdict(dict)
-for i, documento in matriz_distancias[0].items():
-	aux = list()
-	for j, centroide in matriz_distancias.items():
-		aux.append(centroide[i])
-	grupos[i] = aux.index(min(aux))
+	grupos = defaultdict(dict)
+	for i, texto in matriz_distancias[0].items():
+		aux = list()
+		for j, centroide in matriz_distancias.items():
+			aux.append(centroide[i])
+		grupos[i] = aux.index(min(aux))
 
-print(matriz_distancias, grupos)
+	print("%dª interacao\n" % (m))
+	print(grupos)
+
+	for i, centroide in centroides.items():
+		indices_media = list()
+		for j, grupo in grupos.items():
+			if grupo == i:
+				#guarda os indices das posicoes que serao usadas pro calculo da media
+				indices_media.append(j)
+		aux = list()
+		for k, dimensao_centroide in centroide.items():
+			for indice_media in indices_media:
+				valor = round(float(dados[indice_media][k]), 2)
+				aux.append(valor)
+
+			media = round(numpy.average(aux), 2)
+			centroide[k] = media
