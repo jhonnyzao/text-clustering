7c30eed34371a6764c5d220e331719705a723b11
Joao Armando <joaoarmando94@gmail.com>
2018-05-15 00:53:03 -0300

    Metodo que inicializa centroides aleatoriamente e implementacao da condicao de parada

--- teste.py
+++ teste.py
@@ -9,7 +9,7 @@ iteracoes_maximas = 1000
 convergencia = 0.01
 total_k = 3
 
-def inicializa_centroides(dados, total_k):
+def inicializa_centroides_aleatoriamente(dados, total_k):
 	centroides = defaultdict(dict)
 	valores_maximos = list()
 	valores_minimos = list()
@@ -25,12 +25,11 @@ def inicializa_centroides(dados, total_k):
 		for coluna in range(0, len(dados[0])):
 			centroides[linha][coluna] = randint(int(valor_minimo), int(valor_maximo))
 
-	print(centroides)
-	exit()
+	c = defaultdict(dict)
 	
-	#centroides[0] = {0: '2', 1: '1', 2: '2', 3: '1', 4: '1'}
-	#centroides[1] = {0: '3', 1: '1', 2: '0', 3: '0', 4: '0'}
-	#centroides[2] = {0: '2', 1: '0', 2: '1', 3: '2', 4: '1'}
+	c[0] = {0: '2', 1: '1', 2: '2', 3: '1', 4: '1'}
+	c[1] = {0: '3', 1: '1', 2: '0', 3: '0', 4: '0'}
+	c[2] = {0: '2', 1: '0', 2: '1', 3: '2', 4: '1'}
 
 	return centroides
 
@@ -59,39 +58,49 @@ with open('textos.csv') as arquivo:
 		for j, value in enumerate(row):
 			dados[i][j] = value
 
-centroides = inicializa_centroides(dados, total_k)
+centroides = inicializa_centroides_aleatoriamente(dados.copy(), total_k)
 
-for m in range(0, 10):
+grupos = defaultdict(dict)
+grupos_ultima_iteracao = defaultdict(dict)
+convergiu = False
+iteracao_atual = 0
+
+while (iteracao_atual <= iteracoes_maximas or not convergiu):
 	matriz_distancias = defaultdict(dict)
 	for i, centroide in centroides.items():
 		for j, dado in dados.items():
-			#print(j, dado)
 			matriz_distancias[i][j] = distancia_euclidiana(centroide, dado)
 
-	grupos = defaultdict(dict)
+	grupos_ultima_iteracao = grupos.copy()
+
 	for i, texto in matriz_distancias[0].items():
 		aux = list()
 		for j, centroide in matriz_distancias.items():
 			aux.append(centroide[i])
 		grupos[i] = aux.index(min(aux))
 
-	print("%dª interacao\n" % (m))
-	#print(grupos)
+	#compara se algum dado mudou de centroide e assume convergencia em caso negativo
+	if grupos_ultima_iteracao == grupos:
+		convergiu = True
+		break
 
+	print("%dª iteracao\n" % (iteracao_atual))
+	
 	for i, centroide in centroides.items():
 		indices_media = list()
 		for j, grupo in grupos.items():
 			if grupo == i:
 				#guarda os indices das posicoes que serao usadas pro calculo da media
 				indices_media.append(j)
-
+		
 		for k, dimensao_centroide in centroide.items():
 			aux = list()
 			for indice_media in indices_media:
 				valor = dados[indice_media][k]
 				aux.append(int(valor))
 
-			media = round(numpy.average(aux), 2)
-			centroide[k] = media
-
+			if aux:
+				media = round(numpy.average(aux), 2)
+				centroide[k] = media
 	print(grupos)
+	iteracao_atual += 1
